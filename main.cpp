9b47adbef00b89a22f4b9aa99fd6be2cc577c685
Henry <henry@outlook.fr>
2022-03-10 09:08:25 +0100

    Add Qt.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+template <typename T>
+T add(T num1, T num2) {
+   return (num1 + num2);
+}
+
+template <typename T>
+T Sub(T a, T b) {
+   return a-b;
+}
+
+template <typename T>
+T Div(T a, T b) {
+   return a/b;
+}
+
+void main() {
+
+}
